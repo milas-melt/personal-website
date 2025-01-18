32b43a21f131d99fe581901ee51b63c60f711f75
milas-melt <tlemcani.salim00@gmail.com>
2025-01-18 01:19:45 +0000

    added job xp

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -189,7 +189,7 @@ function Resume() {
     {
       company:
         'Imperial College London - Artificial Intelligence Network / Imperial Business Analytics',
-      title: 'Research Assistant',
+      title: 'Research Assistant - non-parametric statistics',
       start: '2022',
       end: '2023',
     },
